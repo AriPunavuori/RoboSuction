4ce6c50cab3cddd77624868df4a878c9b413b915
Ari Punavuori <ari.punavuori@gmail.com>
2019-04-27 18:59:09 +0300

    Bot movement

--- RoboSuction/Assets/Scripts/EnemyController.cs
+++ RoboSuction/Assets/Scripts/EnemyController.cs
@@ -6,11 +6,11 @@ public class EnemyController : MonoBehaviour {
 
     public float moveSpeed = 10f;
     public float hoverHeight = 0.75f;
-    public float turnSpeed = 10f;
+    public float turnSpeed = 60f;
     public float flipSpeed = 90f;
     public float enemyWidth = 0.75f;
     public float stunDist = 0.1f;
-    public float upDetection;
+    public float dirDetectionDelta;
     public bool huntMode;
     public bool stunned;
     public int health = 3;
@@ -35,6 +35,7 @@ public class EnemyController : MonoBehaviour {
         } else {
             if(Vector3.Distance(prevPos, transform.position) < stunDist && Quaternion.Angle(transform.rotation, prevRot) < 1f) {
                 rb.useGravity = false;
+                rb.isKinematic = true;
                 stunned = false;
             }
             if(!stunned) {
@@ -46,25 +47,33 @@ public class EnemyController : MonoBehaviour {
     }
 
     void Flip() {
-        if(Vector3.Distance(transform.forward, Vector3.up) < upDetection) {
+        if(Vector3.Distance(transform.forward, Vector3.up) < dirDetectionDelta) {
             targetRot = Quaternion.LookRotation(Vector3.ProjectOnPlane(transform.up, Vector3.up), Vector3.up);
-        } else if (Vector3.Distance(transform.forward, Vector3.down) < upDetection) {
+        } else if(Vector3.Distance(transform.forward, Vector3.down) < dirDetectionDelta) {
             targetRot = Quaternion.LookRotation(Vector3.ProjectOnPlane(-transform.up, Vector3.up), Vector3.up);
         } else {
             targetRot = Quaternion.LookRotation(Vector3.ProjectOnPlane(transform.forward, Vector3.up), Vector3.up);
         }
         rb.rotation = Quaternion.RotateTowards(rb.rotation, targetRot, flipSpeed * Time.deltaTime);
-        if(Vector3.Distance(transform.up, Vector3.up) < upDetection) {
+        if(Vector3.Distance(transform.up, Vector3.up) < dirDetectionDelta) {
             rb.rotation = targetRot;
             huntMode = true;
         }
     }
 
     void Move() {
-        var targetVector = player.position - transform.position;
-        if(targetVector.magnitude > enemyWidth) {
-            var targetPos = transform.position + targetVector.normalized * moveSpeed * Time.deltaTime;
+        targetRot = Quaternion.LookRotation(Vector3.ProjectOnPlane((player.transform.position - transform.position),Vector3.up), Vector3.up);
+        if(transform.position.y < hoverHeight) {
+            var targetPos = transform.position + Vector3.up * hoverHeight * moveSpeed * Time.deltaTime;
             rb.MovePosition(targetPos);
+        } else if(Vector3.Distance(transform.forward, Vector3.Normalize(Vector3.ProjectOnPlane(player.transform.position - transform.position, Vector3.up))) > dirDetectionDelta) {
+            rb.rotation = Quaternion.RotateTowards(rb.rotation, targetRot, turnSpeed * Time.deltaTime);
+        } else {
+            var targetVector = Vector3.ProjectOnPlane(player.position - transform.position, Vector3.up);
+            if(targetVector.magnitude > enemyWidth) {
+                var targetPos = transform.position + targetVector.normalized * moveSpeed * Time.deltaTime;
+                rb.MovePosition(targetPos);
+            }
         }
     }
 
@@ -73,7 +82,9 @@ public class EnemyController : MonoBehaviour {
         if(other.gameObject.layer == batLayer) {
             huntMode = false;
             stunned = true;
+            rb.isKinematic = false;
             rb.useGravity = true;
+
             health -= 1;
             if(health < 1)
                 Destroy(gameObject, 1f);

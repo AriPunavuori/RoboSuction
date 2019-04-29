bf04dfadd4f2ba48c2be99f94dec3047ae9d1182
Ari Punavuori <ari.punavuori@gmail.com>
2019-04-29 16:47:13 +0300

    Patonki v2.0

--- RoboSuction/Assets/Scripts/Patonki.cs
+++ RoboSuction/Assets/Scripts/Patonki.cs
@@ -11,23 +11,6 @@ public class Patonki : MonoBehaviour {
     public float feedbackAmplitude;
     public float feedbackFrequency;
     public float feedbackLength;
-    Rigidbody rb;
-<<<<<<< HEAD
-
-    private void Awake() {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    private void FixedUpdate() {
-=======
-    private void Awake() {
-        rb = GetComponent<Rigidbody>();
-    }
-    private void FixedUpdate() {
->>>>>>> 531dcba4b98022a505dd7a1f521317a5d5919e46
-        rb.rotation = attachmentPoint.rotation;
-        rb.position = attachmentPoint.position;
-    }
 
     private void OnCollisionEnter(Collision collision) {
         if(interactable.attachedToHand != null) {

270e2398edac7bfc0e70d9936e5c51f7a8b9f4e0
AriPunavuori <ari.punavuori@gmail.com>
2019-04-29 17:35:45 +0300

    >Arin muutokset + PILARIT

--- RoboSuction/Assets/Scripts/Patonki.cs
+++ RoboSuction/Assets/Scripts/Patonki.cs
@@ -8,15 +8,27 @@ public class Patonki : MonoBehaviour {
     public Transform attachmentPoint;
     public SteamVR_Action_Vibration touchFeedback;
     Interactable interactable;
-    public float feedbackAmplitude;
-    public float feedbackFrequency;
-    public float feedbackLength;
+    public float feedbackAmplitude = 100;
+    public float feedbackFrequency = 100;
+    public float feedbackLength  = 1;
 
-    private void OnCollisionEnter(Collision collision) {
-        if(interactable.attachedToHand != null) {
+    //private void OnCollisionEnter(Collision collision) {
+    //    if(interactable.attachedToHand != null) {
+    //        touchFeedback.Execute(0, feedbackLength, feedbackFrequency, feedbackAmplitude, SteamVR_Input_Sources.RightHand);
+    //        print("Feedback");
+    //    }
+    //}
+    private void Awake()
+    {
+        interactable = GetComponent<Interactable>();    
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
             touchFeedback.Execute(0, feedbackLength, feedbackFrequency, feedbackAmplitude, SteamVR_Input_Sources.RightHand);
             print("Feedback");
-        }
+        
     }
 
 }
